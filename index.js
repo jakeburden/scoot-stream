012cef8a46fb09e531f645ac5121272e19286914
jekrb <Jacob.JW.Burden@gmail.com>
2016-07-09 10:28:15 -0400

    ES5ify

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-module.exports = prop => (stream, msg) => {
-  if (!prop) throw new Error('scoot-stream requires a default property')
-  if (!stream) throw new Error('scoot-stream requires a stream')
+module.exports = function (prop) {
+  return function (stream, msg) {
+    if (!prop) throw new Error('scoot-stream requires a default property')
+    if (!stream) throw new Error('scoot-stream requires a stream')
 
-  if (typeof msg === 'string') {
-    const obj = {}
-    obj[prop] = msg
-    stream.write(`${JSON.stringify(obj)}
-`)
-  } else if (msg !== null && typeof msg === 'object' && !(Array.isArray(msg))) {
-    stream.write(`${JSON.stringify(msg)}
-`)
-  } else throw new Error('scoot-stream requires a message string or object.')
+    if (typeof msg === 'string') {
+      var obj = {}
+      obj[prop] = msg
+      stream.write(JSON.stringify(obj) + '\n')
+    } else if (msg !== null && typeof msg === 'object' && !(Array.isArray(msg))) {
+      stream.write(JSON.stringify(msg) + '\n')
+    } else throw new Error('scoot-stream requires a message string or object.')
+  }
 }
